9f7504989ee2d083fd4531dbd6017bc9a4281f60
no-im-bk <80921911+no-im-bk@users.noreply.github.com>
2025-02-03 20:45:40 -0700

    added search

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,11 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    println!("{contents}");
+    let results = search(&config.query, &contents);
+
+    for line in results {
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -22,5 +26,33 @@ impl Config {
         }
 
         Ok(Config {query: args[1].clone(), file_path: args[2].clone()})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
